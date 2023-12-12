6b32432d75e15427d1f9750b655281c63f6711ba
alexgabriel8 <alexgabriels8@outlook.com>
2023-12-12 06:41:35 -0300

    Game state equal to "Criando os cards dinamicamente"

---
+++ src/scripts/engine.js
@@ -0,0 +1,29 @@
+const emojis = [
+    "🕹",
+    "🕹",
+    "💻",
+    "💻",
+    "💾",
+    "💾",
+    "⌨",
+    "⌨",
+    "🖱",
+    "🖱",
+    "👨‍💻",
+    "👨‍💻",
+    "💿",
+    "💿",
+    "🖨",
+    "🖨"
+];
+
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for(let i=0; i < emojis.length; i++) {
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box);
+}
